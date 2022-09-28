78dc4f573a5954695bff1185bf457e08b632152f
leonor.perez.contractor.gdevtools.com <leonor.perez.contractor@gdevtools.com>
2022-09-27 21:59:32 -0300

    Se agrega login, logout y estilos

--- App.py
+++ App.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_mysqldb import MySQL
+import MySQLdb.cursors
 
 
 app = Flask(__name__)
@@ -14,13 +15,66 @@ mysql = MySQL(app)
 
 app.secret_key = 'mysecretkey'
 
-@app.route('/')
+
+@app.route('/', methods = ['GET', 'POST'])
+def login():
+    global user
+    error = ''
+    
+    if request.method == 'POST':
+        usuario = request.form['usuario']
+        password = request.form['password']
+        
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM usuarios WHERE usuario = %s AND password = %s', (usuario, password,))
+        user = cursor.fetchone()
+
+        if user:
+            session['loggedin'] = True
+            return redirect(url_for('Index'))       
+        else:  
+            error = '¡Usuario/contraseña Incorrecto!'
+    return render_template('login.html', error = error)
+   
+
+@app.route('/logout')
+def logout():
+    global user
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('usuario', None)
+    user = {}
+    return redirect(url_for('login'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    error = ''
+   
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        usuario = request.form['usuario']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM usuarios WHERE usuario = %s', (usuario,))
+        account = cursor.fetchone()
+        
+        if account:
+            error = '¡El usuario ya existe!'
+        else:                     
+            cursor.execute('INSERT INTO usuarios VALUES (null, %s, %s, %s, %s)', (nombre, apellido, usuario, password,))
+            mysql.connection.commit()
+            msg = '¡Usuario registrado correctamente!'
+            return render_template('login.html', msg = msg)
+    return render_template('register.html', error = error)
+
+@app.route('/candidatos')
 def Index():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM candidatos')
     data = cur.fetchall()
     return render_template('index.html', candidatos = data)
-
 
 @app.route('/add_contact', methods = ['POST'])    
 def add_contact():
@@ -79,4 +133,4 @@ def delete_contact(idcandidato):
 
 
 if(__name__=='__main__'):
-    app.run(port = 3006, debug = True) 
+    app.run(port = 3007, debug = True) 
